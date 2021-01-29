32a57f0b111cd0123bf7060e525ade9633b7f46d
clemetayer <c.lemetayer@laposte.net>
2021-01-29 22:42:33 +0100

    Added Light powerup

---
+++ Assets/Scripts/Collectibles/CollectibleLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleLight : CollectibleItem
+{
+
+    public Light light;
+
+    public void Start()
+    {
+        light.enabled = false;
+    }
+
+    public override void collectibleMethod()
+    {
+        light.enabled = true;
+    }
+}
