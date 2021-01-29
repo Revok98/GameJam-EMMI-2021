c6344aa1232e4d5bd04824128cbbcc0f6bf026fa
clemetayer <c.lemetayer@laposte.net>
2021-01-29 22:00:23 +0100

    Added sound when getting collectible

--- Assets/Scripts/Collectibles/CollectibleItem.cs
+++ Assets/Scripts/Collectibles/CollectibleItem.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public abstract class CollectibleItem : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     private void OnTriggerEnter(Collider element)
     {
         if (element.tag == "CanCollect")
         {
+            if(audioSource != null) audioSource.Play();
             Destroy(gameObject);
             collectibleMethod();
         }
