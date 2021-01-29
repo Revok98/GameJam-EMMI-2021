a2f4ede252047db1d4d47995ad2a727f3fb5985b
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2021-01-29 19:53:46 +0100

    Add camera iso

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject target;
+    private Vector3 delta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delta = transform.position - target.transform.position;
+    }
+      
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + delta;
+    }
+}
