f6d81432a14e15a960da7723fed5043a7e89fb18
Eymeric Poinsignon <Eymeric.Poinsignon@bordeaux-inp.fr>
2021-01-29 23:31:02 +0100

    See through outside walls

---
+++ Assets/Scripts/GoToNextLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoToNextLevel : MonoBehaviour
+{
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName: name);
+        }
+    }
+}
---
+++ Assets/Scripts/SeeThroughWall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThroughWall : MonoBehaviour
+{
+    List<GameObject> DeactivatedWalls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactivatedWalls = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<GameObject> UpdatedDeactivatedWalls = DeactivatedWalls;
+        GameObject player = GameObject.Find("Player");
+        RaycastHit hit;
+        Physics.Raycast(gameObject.transform.position, -(gameObject.transform.position - player.transform.position).normalized, out hit, Mathf.Infinity);
+        if (hit.collider.gameObject.tag == "Wall")
+        {
+            hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            DeactivatedWalls.Add(hit.collider.gameObject);
+        }
+        else {
+            for (int i = 0; i < DeactivatedWalls.Count; i++)
+            {
+                DeactivatedWalls[i].GetComponent<MeshRenderer>().enabled = true;
+                UpdatedDeactivatedWalls.Remove(DeactivatedWalls[i]);
+            }
+            DeactivatedWalls = UpdatedDeactivatedWalls;
+        }
+
+    }
+}
