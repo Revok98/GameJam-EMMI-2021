5c827dd65c8d7681aa46433c0f76254e151e14fd
clemetayer <c.lemetayer@laposte.net>
2021-01-30 19:14:19 +0100

    Updated spawners

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -7,12 +7,13 @@ using UnityEngine.AI;
 public class World : MonoBehaviour
 {
     public GameObject agent;
+    public GameObject terrain;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<NavMeshSurface>().BuildNavMesh();
-        Instantiate(agent, new Vector3(9,0,9), Quaternion.identity);
+        terrain.GetComponent<NavMeshSurface>().BuildNavMesh();
+        Instantiate(agent, this.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
