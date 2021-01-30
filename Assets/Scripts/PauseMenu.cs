6d2cc0089a71abe2164c2a219747cf9c201463b4
Clari <quesseveur.nicolas@gmail.com>
2021-01-30 14:53:35 +0100

    Pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject Button;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("LA");
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    { 
+        Debug.Log(gameIsPaused);
+        if (gameIsPaused)
+        {
+            Debug.Log("IF");
+            GetComponent<Image>().enabled = true;
+            Time.timeScale = 0f;
+            Button.SetActive(true);
+        }
+        else
+        {
+            GetComponent<Image>().enabled = false;
+            Time.timeScale = 1;
+            Button.SetActive(false);
+        }
+    }
+}
