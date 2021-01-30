5822d46279d8223a83b6d3c5e7a92d60d702da09
Eymeric Poinsignon <Eymeric.Poinsignon@bordeaux-inp.fr>
2021-01-30 17:14:23 +0100

    Rangement et clé sur l'interface

--- Assets/Scripts/Collectibles/CollectibleKey.cs
+++ Assets/Scripts/Collectibles/CollectibleKey.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CollectibleKey : CollectibleItem
 {
-
+    public Image image;
     // Usage : Mettre la porte en paramètre
     public DoorController Door;
 
@@ -16,5 +16,8 @@ public class CollectibleKey : CollectibleItem
     public override void collectibleMethod()
     {
         Door.locked = false;
+
+        GameObject KeyImage = GameObject.Find("KeyImage");
+        KeyImage.GetComponent<Image>().enabled = true;
     }
 }
