93c948ba283b5653e583276b55c9a1285e818613
clemetayer <c.lemetayer@laposte.net>
2021-01-30 10:17:41 +0100

    Added collectible sounds sprite

---
+++ Assets/Scripts/Collectibles/CollectibleSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CollectibleSound : CollectibleItem
+{
+
+    private GameObject[] monsters;
+    public Image image;
+    public Canvas canvas;
+
+    public override void collectibleMethod()
+    {
+        monsters = GameObject.FindGameObjectsWithTag("Ennemy");
+        foreach(GameObject monster in monsters)
+        {
+            addSoundSprite(monster);
+        }
+    }
+
+    private void addSoundSprite(GameObject monster)
+    {
+        CameraSpriteFollow csf = monster.AddComponent<CameraSpriteFollow>();
+        Image imageInstance = Instantiate(image, new Vector3(0, 0, 0), Quaternion.identity);
+        imageInstance.transform.parent = canvas.transform;
+        csf.image = imageInstance;
+    }
+}
