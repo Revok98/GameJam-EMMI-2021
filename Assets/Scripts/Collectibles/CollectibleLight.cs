b966a720ee77b886fd049cc351b05e6112ea9b9f
Eymeric Poinsignon <Eymeric.Poinsignon@bordeaux-inp.fr>
2021-01-30 16:03:48 +0100

    Inventory finished with activable light

--- Assets/Scripts/Collectibles/CollectibleLight.cs
+++ Assets/Scripts/Collectibles/CollectibleLight.cs
@@ -7,14 +7,17 @@ public class CollectibleLight : CollectibleItem
 
     // Usage : ajouter une directional light (ou une light quelconque) dans la scène et le placer en paramètre 
     public Light light;
+    float lightTime = 0;
 
     public void Start()
     {
         light.enabled = false;
+        longterm = true;
     }
 
     public override void collectibleMethod()
     {
         light.enabled = true;
+        GameObject.Find("Player").GetComponent<PlayerItems>().lightTime = 10;
     }
 }
--- Assets/Scripts/PlayerItems.cs
+++ Assets/Scripts/PlayerItems.cs
@@ -6,7 +6,9 @@ public class PlayerItems : MonoBehaviour
 {
     List<CollectibleItem> listItems = new List<CollectibleItem>();
     // Start is called before the first frame update
-    public Image image;
+    public List<Image> images;
+    List<Image> drawn = new List<Image>();
+    public float lightTime = 0;
     void Start()
     {
         
@@ -15,10 +17,22 @@ public class PlayerItems : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Light light = GetComponentInChildren<Light>();
+        if (lightTime > 0)
+        {
+            lightTime -= Time.deltaTime;
+        }
+        else
+        {
+            light.enabled = false;
+        }
+
         if (Input.GetKeyDown("1"))
         {
             if (listItems.Count > 0) {
                 listItems[0].collectibleMethod();
+                listItems.RemoveAt(0);
+                UpdateInterface();             
             }
         }
 
@@ -27,6 +41,8 @@ public class PlayerItems : MonoBehaviour
             if (listItems.Count > 1)
             {
                 listItems[1].collectibleMethod();
+                listItems.RemoveAt(1);
+                UpdateInterface();
             }
         }
 
@@ -35,6 +51,8 @@ public class PlayerItems : MonoBehaviour
             if (listItems.Count > 2)
             {
                 listItems[2].collectibleMethod();
+                listItems.RemoveAt(2);
+                UpdateInterface();
             }
         }
 
@@ -43,25 +61,63 @@ public class PlayerItems : MonoBehaviour
             if (listItems.Count > 3)
             {
                 listItems[3].collectibleMethod();
+                listItems.RemoveAt(3);
+                UpdateInterface();
             }
         }
     }
 
     public void CollectItem(CollectibleItem obj) {
+        Image image;
+        if(obj.GetType() == typeof(CollectibleLight))
+        {
+            image = images[1];
+        }
+        else if (obj.GetType() == typeof(CollectibleSpeed))
+        {
+            image = images[0];
+        }
+        else
+        {
+            image = images[0];
+        }
         listItems.Add(obj);
         Image draw = Instantiate(image, new Vector3(0, 0, 0), Quaternion.identity);
         GameObject inventory = GameObject.Find("Inventory");
         draw.transform.SetParent(inventory.transform, false);
         draw.transform.localPosition = new Vector3(-37 + 25 * (listItems.Count -1), -1, 0);
         draw.enabled = true;
+        drawn.Add(draw);
     }
 
     void UpdateInterface()
     {
-        Image draw = Instantiate(image, new Vector3(0, 0, 0), Quaternion.identity);
-        GameObject inventory = GameObject.Find("Inventory");
-        draw.transform.SetParent(inventory.transform, false);
-        draw.transform.localPosition = new Vector3(-37 + 25 * (listItems.Count - 1), -1, 0);
-        draw.enabled = true;
+        for (int i = 0; i < drawn.Count; i++)
+        {
+            drawn[i].enabled = false;
+        }
+        for (int i = 0; i < listItems.Count; i++)
+        {
+            Image image;
+            if (listItems[i].GetType() == typeof(CollectibleLight))
+            {
+                image = images[1];
+            }
+            else if (listItems[i].GetType() == typeof(CollectibleSpeed))
+            {
+                image = images[0];
+            }
+            else
+            {
+                image = images[0];
+            }
+            Debug.Log(listItems[i].GetType());
+            Image draw = Instantiate(image, new Vector3(0, 0, 0), Quaternion.identity);
+            GameObject inventory = GameObject.Find("Inventory");
+            draw.transform.SetParent(inventory.transform, false);
+            draw.transform.localPosition = new Vector3(-37 + 25 * i, -1, 0);
+            draw.enabled = true;
+            drawn.Add(draw);
+        }
     }
 }
